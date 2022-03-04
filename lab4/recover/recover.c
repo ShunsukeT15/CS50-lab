0ad82bd84951331bc325cf6edd83a53881957de7
ShunsukeT15 <sukeo.t111560@gmail.com>
2022-03-04 22:54:40 +0900

    first commit

---
+++ lab4/recover/recover.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    typedef uint8_t BYTE;
+
+     if (argc != 2)
+    {
+        printf("Usage:./recover image\n");
+        return 1;
+    }
+}
